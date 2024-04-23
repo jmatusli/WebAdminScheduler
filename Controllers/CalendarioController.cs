67c31a1efd48a6cea2eef3d6beb87baf6b69eded
Karen Aguirre Tellez <karen.aguirre@tigo.com.ni>
2024-04-22 21:26:32 -0600

    Avances al editar

--- Controllers/CalendarioController.cs
+++ Controllers/CalendarioController.cs
@@ -38,7 +38,7 @@ namespace WebAdminScheduler.Controllers
 		{
            CP_CRONTAB crontabt = new CP_CRONTAB();
            
-            crontabt.IDCRONTAB=1000;
+            crontabt.IDCRONTAB=1001;
             crontabt.FECHA="00000000";
             crontabt.HORA_INICIO="0000";
             crontabt.HORA_FIN="0000";
@@ -53,76 +53,117 @@ namespace WebAdminScheduler.Controllers
             Console.WriteLine("ejemplo "+crontabt.IDCRONTAB);
             return Json(crontabt);
 		}
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _DBContext.CP_CRONTABS == null)
+            {
+                return NotFound();
+            }
 
- 
+            var calendario = await _DBContext.CP_CRONTABS.FindAsync(id);
+            if (calendario == null)
+            {
+                return NotFound();
+            }
+            return View(calendario);
+        }
+
+/*   REVISAR EDIT 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IDCRONTAB,HORA_INICIO,HORA_FIN,WDAY_M2S_EX,REPEAT_EVERY_MINS,REPEAT_AFTER_FINISH")] calendario CP_CRONTABS)
+        {
+            if (id != CP_CRONTABS.IDCRONTAB)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _DBContext.Update(CP_CRONTABS);
+                    await _DBContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CrontabsExists(CP_CRONTABS.IDCRONTAB))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(calendario);
+        }
+    */
+        private bool CrontabsExists(int id)
+        {
+          return (_DBContext.CP_CRONTABS?.Any(e => e.IDCRONTAB == id)).GetValueOrDefault();
+        }
+
 		[HttpPost]
         public JsonResult ListarCrontabs()
         {
-                int totalRecord = 0;
-    int filterRecord = 0;
-    var draw = Request.Form["draw"].FirstOrDefault();
-    var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
-    var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
-    var searchValue = Request.Form["search[value]"].FirstOrDefault();
-    int pageSize = Convert.ToInt32(Request.Form["length"].FirstOrDefault() ?? "0");
-    int skip = Convert.ToInt32(Request.Form["start"].FirstOrDefault() ?? "0");
-    IQueryable<CP_CRONTAB>  data = _DBContext.Set < CP_CRONTAB > ().AsQueryable();
-    //get total count of data in table
-    totalRecord = data.Count();
-    // search data when search value found
-   /* if (!string.IsNullOrEmpty(searchValue)) {
-        data = data.Where(x => x.FECHA.ToLower().Contains(searchValue.ToLower()) );
-    }*/
-    // get total count of records after search
-      
-    
-   // if (!string.IsNullOrEmpty(sortColumn) && !string.IsNullOrEmpty(sortColumnDirection)) data = data.OrderBy(sortColumn+" "+sortColumnDirection);
-    
-    _DBContext.Database.OpenConnection();
-    OracleCommand oraCommand = new OracleCommand("SELECT * FROM ("
-    +"SELECT cc.*,row_number() over (ORDER BY cc.idcrontab ASC) line_number " 
-    +"FROM APP_SCL_ALTAMIRA.CP_CRONTAB cc) "
-    +"WHERE line_number BETWEEN 0 AND 10  ORDER BY line_number", (OracleConnection)_DBContext.Database.GetDbConnection());
-    //oraCommand.BindByName = true;
-   // oraCommand.Parameters.Add(new OracleParameter("@oStart", "0"));
-   OracleDataReader oraReader = oraCommand.ExecuteReader();
-   List<CP_CRONTAB> cp_contrabList = new List<CP_CRONTAB>();
-   
- 
-    if (oraReader.HasRows)
-    {
-        while (oraReader.Read())
-        {
-             CP_CRONTAB cp_crontab = new CP_CRONTAB();
-            cp_crontab.IDCRONTAB = oraReader.GetInt32(0);
-            cp_crontab.FECHA = oraReader.GetString(1);
-            cp_crontab.HORA_INICIO = oraReader.GetString(2);
-            cp_crontab.RECURRENCIA= oraReader.GetString(3);
-            cp_crontab.HORA_FIN= oraReader.GetString(4);
-            cp_crontab.WDAY_M2S_EX= oraReader.GetString(5);
-            cp_crontab.DAY_EX= oraReader.GetString(6);
-            cp_crontab.MONTH_EX= oraReader.GetString(7);
-            cp_crontab.REPEAT_EVERY_MINS= oraReader.GetInt32(8);
-            cp_crontab.REPEAT_AFTER_FINISH= oraReader.GetInt32(8);
-             cp_contrabList.Add(cp_crontab);
+            int totalRecord = 0;
+            int filterRecord = 0;
+            var draw = Request.Form["draw"].FirstOrDefault();
+            var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+            var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+            var searchValue = Request.Form["search[value]"].FirstOrDefault();
+            int pageSize = Convert.ToInt32(Request.Form["length"].FirstOrDefault() ?? "0");
+            int skip = Convert.ToInt32(Request.Form["start"].FirstOrDefault() ?? "0");
+            IQueryable<CP_CRONTAB>  data = _DBContext.Set < CP_CRONTAB > ().AsQueryable();
+            //get total count of data in table
+            totalRecord = data.Count();
             
-        }
-        filterRecord = cp_contrabList.Count();
-   
-    }
-    else
-    {
-        return Json(null);
-    }
+            _DBContext.Database.OpenConnection();
+            OracleCommand oraCommand = new OracleCommand("SELECT * FROM ("
+            +"SELECT cc.*,row_number() over (ORDER BY cc.idcrontab ASC) line_number " 
+            +"FROM APP_SCL_ALTAMIRA.CP_CRONTAB cc) "
+            +"WHERE line_number BETWEEN 0 AND 10  ORDER BY line_number", (OracleConnection)_DBContext.Database.GetDbConnection());
 
-    oraReader.Close();
-    _DBContext.Database.CloseConnection();
-    var returnObj = new {
-        draw = draw, recordsTotal = totalRecord, recordsFiltered = filterRecord, data = cp_contrabList
-    };
-     
-     return Json(returnObj); 
-    }
+            OracleDataReader oraReader = oraCommand.ExecuteReader();
+            List<CP_CRONTAB> cp_contrabList = new List<CP_CRONTAB>();
+    
+            if (oraReader.HasRows)
+            {
+                while (oraReader.Read())
+                {
+                    CP_CRONTAB cp_crontab = new CP_CRONTAB();
+                    cp_crontab.IDCRONTAB = oraReader.GetInt32(0);
+                    cp_crontab.FECHA = oraReader.GetString(1);
+                    cp_crontab.HORA_INICIO = oraReader.GetString(2);
+                    cp_crontab.RECURRENCIA= oraReader.GetString(3);
+                    cp_crontab.HORA_FIN= oraReader.GetString(4);
+                    cp_crontab.WDAY_M2S_EX= oraReader.GetString(5);
+                    cp_crontab.DAY_EX= oraReader.GetString(6);
+                    cp_crontab.MONTH_EX= oraReader.GetString(7);
+                    cp_crontab.REPEAT_EVERY_MINS= oraReader.GetInt32(8);
+                    cp_crontab.REPEAT_AFTER_FINISH= oraReader.GetInt32(8);
+                    cp_contrabList.Add(cp_crontab);
+                }
+                filterRecord = cp_contrabList.Count();
+        
+            }
+            else
+            {
+                return Json(null);
+            }
+
+            oraReader.Close();
+            _DBContext.Database.CloseConnection();
+            var returnObj = new {
+                draw = draw, recordsTotal = totalRecord, recordsFiltered = filterRecord, data = cp_contrabList
+            };
+        
+            return Json(returnObj); 
+        }
 }
 
 }
