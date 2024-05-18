ce39dcb10c78392ec240d5b6ec78416e5e76bd65
jamatusli <jamatusli@gmail.com>
2024-05-18 00:48:27 -0600

    agregando dependencias de proceso al guardado del job

--- Helpers/WACustomeHelper.cs
+++ Helpers/WACustomeHelper.cs
@@ -13,15 +13,23 @@ namespace WebAdminScheduler.helpers
       {
          CP_CRONTAB data = (from s in WAContext.CP_CRONTABS.OrderByDescending(x => x.IDCRONTAB)
                            select s).ToList().AsQueryable().FirstOrDefault();
-                  int LasIdcrontab=data.IDCRONTAB+1;
+                  int LasIdcrontab=(data?.IDCRONTAB?? 0)+1;
                   return LasIdcrontab;
       }
       public static int GetLastIdPROC(WebAdminSchedulerContext WAContext)
       {
          CP_PROCESOS data = (from s in WAContext.CP_PROCESOS.OrderByDescending(x => x.IDPROC)
                            select s).ToList().AsQueryable().FirstOrDefault();
-                  int LastIdproc=data.IDPROC+1;
+                  int LastIdproc=(data?.IDPROC?? 0)+1;
                   return LastIdproc;
+      }
+
+       public static int GetLastIDDEP(WebAdminSchedulerContext WAContext)
+      {
+         CP_DEPENDENCIAS data = (from s in WAContext.CP_DEPENDENCIAS.OrderByDescending(x => x.IDDEP)
+                           select s).ToList().AsQueryable().FirstOrDefault();
+                  int LastIddep= (data?.IDDEP ?? 0)+1;
+                  return LastIddep;
       }
    }
 }
--- Models/ViewModels/ProcesosVM.cs
+++ Models/ViewModels/ProcesosVM.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebAdminScheduler.Models.ViewModels
@@ -5,5 +6,8 @@ namespace WebAdminScheduler.Models.ViewModels
     public class ProcesosVM
     {
         public CP_PROCESOS oProcesos { get; set; }
+
+     
+        public List<CP_DEPENDENCIAS> oDependencias { get; set; }
     }
 }
